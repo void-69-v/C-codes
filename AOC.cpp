edca6a3f03b193b7ca4b17695f51bfd5e7904464
Void <k.void.108@gmail.com>
2026-07-16 01:05:49 +0530

    Add files via upload

---
+++ AOC.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //basically area of circle ke liye code hai
+    float  radius ; int x;
+    cout <<"Enter the radius of circle : ";
+    cin >> radius;
+    x = 3.14159265*radius*radius;
+    cout <<"The area of circle is : "<<x<<endl;
+}
